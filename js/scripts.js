fb2f95d8f02ab8ae4a0d56a4704bc25b0736471f
Justin Golden <justingolden21@gmail.com>
2021-09-08 00:56:30 -0700

    pass in settings (unused)

--- js/scripts.js
+++ js/scripts.js
@@ -4,12 +4,17 @@ quick copy buttons
 autofocus/select on generate
 autofocus/select on page load
 
+url params for settings?
+upload json file for settings?
+
 more:
 pin generator
 truly random (no words) pass generator
 dropdown menu with "memorable" as default
 https://1password.com/password-generator/
 */
+
+// pass item generation
 
 // https://owasp.org/www-community/password-special-characters
 const specials = ` !"#$%&'()*+,-./:;<=>?@[\]^_\`{|}~`.split('');
@@ -33,6 +38,27 @@ const capitalize = (str) =>
 
 const randInt = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
 
+// input validation
+
+function getVal(elmID) {
+	let elm = u(`#${elmID}`);
+	let val = verify(
+		elm.val(),
+		elm.attr('min'),
+		elm.attr('max'),
+		elm.attr('min')
+	);
+	elm.val(val);
+	return val;
+}
+
+function verify(num, min, max, defaultVal) {
+	num = Math.max(Math.min(parseInt(num), max), min);
+	return isNaN(num) ? defaultVal : num;
+}
+
+// pass generation
+
 function getRandomPass(settings) {
 	let pass = '';
 	for (let i = 0; i < settings.size; i++) {
@@ -51,18 +77,38 @@ function getRandomPass(settings) {
 	return pass;
 }
 
-let settings = {
-	size: 5,
-};
-
 window.onload = () => {
 	u('#gen-btn').on('click', () => {
-		const pass = getRandomPass(settings);
-		let html = `<input id="output" class="input my-2" type="text" value="${pass.replace(
-			/\"/g,
-			'&quot;'
-		)}"></input>`; // replace quote with &quot;
-		u('#output').html(html);
+		const numPasswords = getVal('num-passwords');
+		let settings = {
+			size: getVal('size'),
+			generatedItems: {
+				caps: u('#caps-checkbox').is(':checked'),
+				lower: u('#lower-checkbox').is(':checked'),
+				capitalized: u('#capitalized-checkbox').is(':checked'),
+				togglecase: u('#togglecase-checkbox').is(':checked'),
+				number: u('#number-checkbox').is(':checked'),
+				special: u('#special-checkbox').is(':checked'),
+			},
+			seperators: {
+				underscore: u('#underscore-checkbox').is(':checked'),
+				dash: u('#dash-checkbox').is(':checked'),
+			},
+			minWordLen: getVal('min-word-len'),
+			maxWordLen: getVal('max-word-len'),
+			numberSize: getVal('number-size'),
+			specialChars: u('#special-chars').val(),
+		};
+
+		for (let i = 0; i < numPasswords; i++) {
+			console.log(settings);
+			const pass = getRandomPass(settings);
+			let html = `<input id="output" class="input my-2" type="text" value="${pass.replace(
+				/\"/g,
+				'&quot;'
+			)}"></input>`; // replace quote with &quot;
+			u('#output').html(html);
+		}
 	});
 	u('#gen-btn').trigger('click');
 };
